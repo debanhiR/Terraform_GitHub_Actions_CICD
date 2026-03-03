277167c8449da016e910531f5120949afda8f999
debanhiR <d.rosiles.chena@accenture.com>
2026-03-03 15:38:20 -0600

    Update providers.tf
    Updating name for bucket and dynamodb

--- providers.tf
+++ providers.tf
@@ -5,10 +5,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "neeha-s3bucket-date"
+    bucket         = "deb-s3bucket-01april"
     key            = "globalstate/s3/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "neeha-date-dblocks"
+    dynamodb_table = "deb-01april-dblocks"
     encrypt        = true
   }
 }
